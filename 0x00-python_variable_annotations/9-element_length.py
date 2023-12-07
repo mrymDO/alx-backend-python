4032db501113d0ec902d209cc2266483cdabb061
Meryeme <dobli.meryeme@gmail.com>
2023-12-07 13:17:15 -0800

    fix task9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """duck type"""
-from typing import Iterable, Tuple, List
+from typing import Iterable, Tuple, List, Sequence
 
 
-def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Calculate the length of each element in the input list"""
     return [(i, len(i)) for i in lst]
