016dc0be4d576aad8c70b23d913f07a278118b6a
Meryeme <dobli.meryeme@gmail.com>
2023-12-07 14:09:21 -0800

    task12

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""type annotations"""
+from typing import Tuple
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+    """zoom array"""
+    zoomed_in = tuple(item for item in lst for _ in range(factor))
+    return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+zoom_3x = zoom_array(array, 3)
