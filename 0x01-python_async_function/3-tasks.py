48bb4642eee14d5c63a311a143f5f7035f69eff3
Meryeme <dobli.meryeme@gmail.com>
2023-12-11 11:07:11 -0800

    task3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""create task"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """create task"""
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
