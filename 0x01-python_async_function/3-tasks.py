293f6cec6f54460286fdcdabb87d150001dbf3b3
Meryeme <dobli.meryeme@gmail.com>
2023-12-11 11:08:53 -0800

    fix pycodestyle

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,6 +5,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 def task_wait_random(max_delay: int = 10) -> asyncio.Task:
     """create task"""
     loop = asyncio.get_running_loop()
