2f46e1b5fc8bceabd210fbed89ddf288e360eab9
Meryeme <dobli.meryeme@gmail.com>
2023-12-11 11:20:25 -0800

    fix task4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """tasks"""
 import asyncio
+from typing import List
+
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> list:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """multiple tasks"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
