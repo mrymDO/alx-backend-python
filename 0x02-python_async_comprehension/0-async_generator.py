a8846a1553731e7e6c298eff576631767351624e
Meryeme <dobli.meryeme@gmail.com>
2023-12-12 10:44:01 -0800

    task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator():
     """async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
