bab0bd53efb61bd12e46017c3450449031037143
Meryeme <dobli.meryeme@gmail.com>
2023-12-12 11:39:32 -0800

    task2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""run time for parallel comprehension"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure runtime"""
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+    )
+    end_time = asyncio.get_event_loop().time()
+
+    return end_time - start_time
