1bd55f63e6e026ffc5c6c214ab6fcb22ddb71441
Meryeme <dobli.meryeme@gmail.com>
2024-01-08 11:04:54 -0800

    task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Parameterize and patch as decorators"""
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 
@@ -18,6 +18,16 @@ class TestGithubOrgClient(unittest.TestCase):
         client.org
         url = "https://api.github.com/orgs/{}".format(org)
         mock.assert_called_once_with(url)
+
+    @parameterized.expand([
+        ("random-url", {"repos_url": "http://some_url.com"})
+        ])
+    def test_public_repos_url(self, name, result):
+        """Test method returns correct output"""
+        with patch("client.GithubOrgClient.org",
+                   PropertyMock(return_value=result)):
+            response = GithubOrgClient(name)._public_repos_url
+            self.assertEqual(response, result.get("repos_url"))
 
 
 if __name__ == '__main__':
